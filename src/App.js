cffe91919efa4228f2a0df256d88b3867adb57cf
csalamonik <csalamonik@gmail.com>
2020-07-15 10:26:50 +0200

    added result and buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css, ClassNames } from '@emotion/core';
+import styled from '@emotion/styled';
+import React, { createContext, useContext, useReducer } from 'react';
+import { COLOR_PALETTE } from './AppStyle';
 
-function App() {
+const Section = styled.section`
+  background-color: white;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Number = styled.div`
+  border: 1px solid ${COLOR_PALETTE.pastel_green};
+  margin: 20px;
+  padding: 10px; 
+  color: ${COLOR_PALETTE.pastel_green};
+  font-size: 30px;
+  text-align: center;
+`;
+
+const results = css`
+  display: flex;
+`
+
+
+const buttons = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 5px;
+`
+
+
+const Board = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section
+      css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 300px;
+        width: 400px;
+        background-color:${COLOR_PALETTE.background_navy_blue};
+      `}
+    >
+      <div css={results}>
+        <div css={buttons}>
+          <button >+1</button>
+          <button>-1</button>
+        </div>
+        <Number>0</Number>
+      </div>
+      <div css={results}>
+        <Number>1</Number>
+        <div css={buttons}>
+          <button>+1</button>
+          <button>-1</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const App = () => {
+  return (
+    <Section>
+      <Board />
+    </Section>
   );
 }
 
 export default App;
+
